772bbe472a12e9eb9916f2bd0b6d3a8943cf7333
magnusmorthens <magnusfm89@gmail.com>
2020-05-04 15:19:03 +0000

    did some fixes on the products page and css

--- products/views.py
+++ products/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from products.models import Product
 
 # Create your views here.
 def index(request):
     context = {"products": Product.objects.all().order_by("name")}
     return render(request, "products/index.html", context)
+
+# /products/id
+def get_product_by_id(request, id):
+    return render(request, "products/product_details.html", {
+        "product": get_object_or_404(Product, pk=id)
+    })
