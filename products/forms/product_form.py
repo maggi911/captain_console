d30b92934b51a449cca004a790527f727c8b68b6
magnusmorthens <magnusfm89@gmail.com>
2020-05-05 11:09:46 +0000

    create, update, delete product. Added search function but not working yet

---
+++ products/forms/product_form.py
@@ -0,0 +1,28 @@
+from django.forms import ModelForm, widgets
+from django import forms
+from products.models import Product
+
+class ProductUpdateForm(ModelForm):
+    class Meta:
+        model = Product
+        exclude = ["id"]
+        widgets = {
+            "name": widgets.TextInput(attrs={ "class": "form-control"}),
+            "description": widgets.TextInput(attrs={ "class": "form-control"}),
+            "category": widgets.Select(attrs={ "class": "form-control"}),
+            "price": widgets.NumberInput(attrs={ "class": "form-control"}),
+            "in_stock": widgets.CheckboxInput(attrs={ "class": "form-control"})
+        }
+
+class ProductCreateForm(ModelForm):
+    image = forms.CharField(required=True, widget=forms.TextInput(attrs={ "class": "form-control"}))
+    class Meta:
+        model = Product
+        exclude = ["id"]
+        widgets = {
+            "name": widgets.TextInput(attrs={ "class": "form-control"}),
+            "description": widgets.TextInput(attrs={ "class": "form-control"}),
+            "category": widgets.Select(attrs={ "class": "form-control"}),
+            "price": widgets.NumberInput(attrs={ "class": "form-control"}),
+            "in_stock": widgets.CheckboxInput(attrs={ "class": "form-control"})
+        }
--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     # localhost:8000/products
     path('', views.index, name="products-index"),
-    path('<int:id>', views.get_product_by_id, name="product_details")
+    path('<int:id>', views.get_product_by_id, name="product_details"),
+    path('create_product', views.create_product, name="create_product"),
+    path('delete_product/<int:id>', views.delete_product, name="delete_product"),
+    path('update_product/<int:id>', views.update_product, name="update_product")
 ]
