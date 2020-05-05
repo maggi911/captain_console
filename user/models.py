ef48f5327739a491197984d603e4c21385c3318d
Klevis's Mac <user@users-MacBook.lan>
2020-05-05 23:36:42 +0000

    profile  work in progress

--- user/models.py
+++ user/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
+from products.models import Product
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorite_product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    profile_image = models.CharField(max_length=9999)
