8e405ba38cdb9fee2ad6994e315e5e1d9ff66ddf
Klevis Shakaj <klevis16@ru.is>
2020-05-07 23:57:09 +0000

    working on categories

--- products/models.py
+++ products/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 class ProductCategory(models.Model):
     name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.name
 
