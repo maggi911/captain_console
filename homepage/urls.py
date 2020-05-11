2da8b56fcc69b25dbb1be244de01ac680202eb7b
Klevis Shakaj <klevis16@ru.is>
2020-05-11 20:36:51 +0000

    contact

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     # localhost:8000/
     path('', views.index, name="homepage-index"),
+    path('', views.aboutus, name="homepage-aboutus"),
+    path('', views.contactus, name="homepage-contactus"),
 ]
