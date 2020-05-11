7ed513ae67051d62f8721c63785d318cd41d76d3
magnusmorthens <magnusfm89@gmail.com>
2020-05-11 10:11:14 +0000

    Register site, firstname, lastname

---
+++ user/forms/register.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(max_length=32)
+    last_name = forms.CharField(max_length=32)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
+
+
--- user/views.py
+++ user/views.py
@@ -2,17 +2,20 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from user.models import Profile
 from user.forms.profile_form import ProfileForm
+from user.forms.register import RegisterForm
 
 
 # Create your views here.
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(data=request.POST)
+        form = RegisterForm(data=request.POST)
+        #form = UserCreationForm(data=request.POST)
         if form.is_valid():
             form.save()
             return redirect("login")
     return render(request, "user/register.html", {
-        "form": UserCreationForm()
+        "form": RegisterForm()
+        #"form": UserCreationForm()
     })
 
 def profile(request):
