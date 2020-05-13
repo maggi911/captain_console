cf9dcf9456dbffe992c0b1dbd9eff5e2162f7541
magnusmorthens <magnusfm89@gmail.com>
2020-05-13 20:15:36 +0000

    search history

--- history/views.py
+++ history/views.py
@@ -6,9 +6,6 @@ from products.models import Product
 
 def index(request):
     h = {"history": UserHistory.objects.all().filter(user_id=request.user.id)}
-    #products = {"products": Product.objects.all().order_by("id")}
     user_products = [i.product_id for i in h["history"]]
     context = {"products": Product.objects.filter(id__in=user_products)}
-    print(context["products"])
-    #context = [i for i in products["products"] if i.id in user_products]
     return render(request, "history/search_history.html", context)
